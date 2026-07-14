d44ace7c9720ac0b6272529d441e1c22274516da
dev global <shubhamkamatgekar2000@gmail.com>
2026-07-14 15:44:02 +0530

    Stage 0: hello server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const app=express();
+
+const PORT=3000;
+
+app.get("/",(req,res)=>{
+    res.json({
+        "message":"Hello World!"
+    })
+})
+/*
+app.get("/",(req,res)=>{
+    res.json({
+         "name": "Task API", 
+         "version": "1.0", 
+         "endpoints": ["/tasks"] 
+        });
+});
+
+app.get("/health",(req,res)=>{
+    res.json({
+        "status":"ok"
+    });
+});
+*/
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
