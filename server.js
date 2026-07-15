89bddf1586bb2d1533395c1d3402b736accde1ec
dev global <shubhamkamatgekar2000@gmail.com>
2026-07-15 08:45:31 +0530

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const app=express();
 
 const PORT=3000;
 
+const tasks=[{"id":1,"title":"Learning Express","done":false},{"id":2,"title":"Build API","done":true}
+    ,{"id":3,"title":"Push to Github","done":false}];
+
+app.get("/tasks",(req,res)=>{
+    res.json(tasks);
+});
+
+app.get("/tasks/:id",(req,res)=>{
+    const i=parseInt(req.params.id);
+    const task=tasks.find(task=>task.id==i);
+    if(!task)
+    {
+        return res.status(404).json({ "error": `Task ${i} not found` });
+    }
+    res.json(task);
+});
+
 app.get("/",(req,res)=>{
     res.json({
          "name": "Task API", 
