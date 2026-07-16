a0b279898ccc50b309bcf09dc8395792b02167eb
dev global <shubhamkamatgekar2000@gmail.com>
2026-07-16 14:40:51 +0530

    Stage 4: full CRUD

--- server.js
+++ server.js
@@ -59,6 +59,55 @@ app.post("/tasks",(req,res)=>{
     res.status(201).json(newTask);
 });
 
+app.put("/tasks/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+
+    const task = tasks.find(task=>task.id===id);
+
+    if(!task)
+    {
+        return res.status(404).json({
+            "error":`Task ${id} not found`
+        });
+    }
+
+    const {title , done}=req.body;
+
+    if ((title === undefined && done === undefined) ||
+    (title !== undefined && title.trim() === "") ||
+    (done !== undefined && typeof done !== "boolean")) {
+        return res.status(400).json({
+                "error" : "Provide a valid title and/or done value"
+        });
+    }
+
+    if(title!=undefined)
+    {
+        task.title=title.trim();
+    }
+
+    if(done!=undefined)
+        task.done=done;
+
+    res.json(task);
+});
+
+app.delete("/tasks/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+
+    const index=tasks.findIndex(task=>task.id===id);
+    if(index==-1)
+    {
+        return req.status(404).json({
+            "error":`Task ${id} not found`
+        });
+    }
+
+    tasks.splice(index,1);
+
+    res.status(204).send();
+});
+
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 });
